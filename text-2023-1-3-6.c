a9e803a5da26a6b9c6e368006462ae1528df4c31
Gggguns <2523040452@qq.com>
2023-01-12 21:23:47 +0800

    猜名次

---
+++ text-2023-1-3-6.c
@@ -0,0 +1,415 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int a=0, b=0, c=0, d=0, e=0,i=1;
+	switch (i)
+	{
+	case 1:
+		b = 2;
+		if (2 == b)
+		{
+			if (0 == c && (d = 2) == b)
+			{
+				c = 1;
+				d = 0;
+				if (c != 0)
+					d = 3;
+				else
+					c = 5;
+				if (0 == e && (b == 1 || c == 1 || d == 1))
+				{
+					e = 4;
+					a = 15 - (b + c + d + e);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && a <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+				else
+				{
+					a = 1;
+					e = 15 - (b + c + d + a);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && e <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+
+			}
+			else 
+			{
+				d = 2;
+				if (d != 0)
+					c = 5;
+				else
+					d = 3;
+				if (0 == e && (b == 1 || c == 1 || d == 1))
+				{
+					e = 4;
+					a = 15 - (b + c + d + e);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && a <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+				else
+				{
+					a = 1;
+					e = 15 - (b + c + d + a);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && e <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+				a = 0;c = 1;d = 0;e = 0;
+				if (c != 0)
+					d = 3;
+				else
+					c = 5;
+				if (0 == e && (b == 1 || c == 1 || d == 1))
+				{
+					e = 4;
+					a = 15 - (b + c + d + e);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && a <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+				else
+				{
+					a = 1;
+					e = 15 - (b + c + d + a);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && e <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+			}
+		}
+		else
+		{
+			e = 4;
+			if (0 == c && (d = 2) == b)
+			{
+				c = 1;
+				d = 0;
+				if (c != 0)
+					d = 3;
+				else
+					c = 5;
+				if (0 == e && (b == 1 || c == 1 || d == 1))
+				{
+					e = 4;
+					a = 15 - (b + c + d + e);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && a <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+				else
+				{
+					a = 1;
+					e = 15 - (b + c + d + a);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && e <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+
+			}
+			else
+			{
+				d = 2;
+				if (d != 0)
+					c = 5;
+				else
+					d = 3;
+				if (0 == e && (b == 1 || c == 1 || d == 1))
+				{
+					e = 4;
+					a = 15 - (b + c + d + e);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && a <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+				else
+				{
+					a = 1;
+					e = 15 - (b + c + d + a);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && e <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+				a = 0;c = 1;d = 0;e = 0;
+				if (c != 0)
+					d = 3;
+				else
+					c = 5;
+				if (0 == e && (b == 1 || c == 1 || d == 1))
+				{
+					e = 4;
+					a = 15 - (b + c + d + e);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && a <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+				else
+				{
+					a = 1;
+					e = 15 - (b + c + d + a);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && e <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+			}
+		}
+		a = 0;b = 0;c = 0;d = 0;e = 0;
+	case 2:
+		a = 3;
+		if (2 == b)
+		{
+			if (0 == c && (d = 2) == b)
+			{
+				c = 1;
+				d = 0;
+				if (c != 0)
+					d = 3;
+				else
+					c = 5;
+				if (0 == e && (b == 1 || c == 1 || d == 1))
+				{
+					e = 4;
+					a = 15 - (b + c + d + e);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && a <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+				else
+				{
+					a = 1;
+					e = 15 - (b + c + d + a);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && e <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+
+			}
+			else
+			{
+				d = 2;
+				if (d != 0)
+					c = 5;
+				else
+					d = 3;
+				if (0 == e && (b == 1 || c == 1 || d == 1))
+				{
+					e = 4;
+					a = 15 - (b + c + d + e);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && a <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+				else
+				{
+					a = 1;
+					e = 15 - (b + c + d + a);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && e <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+				a = 0;c = 1;d = 0;e = 0;
+				if (c != 0)
+					d = 3;
+				else
+					c = 5;
+				if (0 == e && (b == 1 || c == 1 || d == 1))
+				{
+					e = 4;
+					a = 15 - (b + c + d + e);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && a <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+				else
+				{
+					a = 1;
+					e = 15 - (b + c + d + a);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && e <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+			}
+		}
+		else
+		{
+			e = 4;
+			if (0 == c && (d = 2) == b)
+			{
+				c = 1;
+				d = 0;
+				if (c != 0)
+					d = 3;
+				else
+					c = 5;
+				if (0 == e && (b == 1 || c == 1 || d == 1))
+				{
+					e = 4;
+					a = 15 - (b + c + d + e);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && a <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+				else
+				{
+					a = 1;
+					e = 15 - (b + c + d + a);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && e <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+
+			}
+			else
+			{
+				d = 2;
+				if (d != 0)
+					c = 5;
+				else
+					d = 3;
+				if (0 == e && (b == 1 || c == 1 || d == 1))
+				{
+					e = 4;
+					a = 15 - (b + c + d + e);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && a <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+				else
+				{
+					a = 1;
+					e = 15 - (b + c + d + a);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && e <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+				a = 0;c = 1;d = 0;e = 0;
+				if (c != 0)
+					d = 3;
+				else
+					c = 5;
+				if (0 == e && (b == 1 || c == 1 || d == 1))
+				{
+					e = 4;
+					a = 15 - (b + c + d + e);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && a <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+				else
+				{
+					a = 1;
+					e = 15 - (b + c + d + a);
+					if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && e <= 5)
+					{
+						printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+					}
+				}
+			}
+		}
+		if (0 == c && (d = 2) == b)
+		{
+			c = 1;
+			d = 0;
+			if (c != 0)
+				d = 3;
+			else
+				c = 5;
+			if (0 == e && (b == 1 || c == 1 || d == 1))
+			{
+				e = 4;
+				a = 15 - (b + c + d + e);
+				if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && a <= 5)
+				{
+					printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+				}
+			}
+			else
+			{
+				a = 1;
+				e = 15 - (b + c + d + a);
+				if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && e <= 5)
+				{
+					printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+				}
+			}
+
+		}
+		else
+		{
+			d = 2;
+			if (d != 0)
+				c = 5;
+			else
+				d = 3;
+			if (0 == e && (b == 1 || c == 1 || d == 1))
+			{
+				e = 4;
+				a = 15 - (b + c + d + e);
+				if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && a <= 5)
+				{
+					printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+				}
+			}
+			else
+			{
+				a = 1;
+				e = 15 - (b + c + d + a);
+				if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && e <= 5)
+				{
+					printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+				}
+			}
+			a = 0;c = 1;d = 0;e = 0;
+			if (c != 0)
+				d = 3;
+			else
+				c = 5;
+			if (0 == e && (b == 1 || c == 1 || d == 1))
+			{
+				e = 4;
+				a = 15 - (b + c + d + e);
+				if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && a <= 5)
+				{
+					printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+				}
+			}
+			else
+			{
+				a = 1;
+				e = 15 - (b + c + d + a);
+				if (b != c && b != d && b != e && c != d && c != e && d != e && a != c && a != d && a != e && b != a && e <= 5)
+				{
+					printf("A=%d B=%d C=%d D=%d E=%d\n", a, b, c, d, e);
+				}
+			}
+		}
+		
+	}
+	return 0;
+}
