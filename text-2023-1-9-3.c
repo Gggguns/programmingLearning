02ee977b2abd1f230cca0c799f1498f294b71195
Gggguns <2523040452@qq.com>
2023-01-18 15:30:58 +0800

    摸你strcmp库函数

---
+++ text-2023-1-9-3.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<assert.h>
+int my_strcmp(const char* str1, const char* str2)
+{
+	while (*str1 == *str2)
+	{
+		if (*str1 == '\0')
+			return 0;
+		str1++;
+		str2++;
+	}
+    if (*str1 > *str2)
+		return 1;
+	else
+		return -1;
+}
+int main()
+{
+	char arr1[] = "sdabibjai";
+	char arr2[] = "sdcnbisjcs";
+	int ret=my_strcmp(arr1, arr2);
+	if (ret > 0)
+		printf(">\n");
+	else if (ret == 0)
+		printf("=\n");
+	else
+		printf("<\n");
+	return 0;
+}
