1c58a9ccebf34fcbc0a935799a0aa9acf27e9906
Gggguns <2523040452@qq.com>
2023-02-03 20:03:58 +0800

    BC49 判断两个数的大小关系

---
+++ text-2022-11-1-3-7.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+    int n1, n2;
+    char c, d;
+    do
+    {
+        scanf("%d", &n1);
+        d = getchar();
+        if (d == ' ')
+        {
+            scanf("%d", &n2);
+            if (n1 > n2)
+                printf("%d>%d\n", n1, n2);
+            else if (n1 == n2)
+                printf("%d=%d\n", n1, n2);
+            else
+                printf("%d<%d\n", n1, n2);
+            c = getchar();
+        }
+        else
+            break;
+
+    } while (c == '\n');
+    return 0;
+}
