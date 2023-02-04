c0a212197e14a572dfff6afa8dfed1ec34ea3dd6
Gggguns <2523040452@qq.com>
2023-02-05 00:04:06 +0800

    BC23 时间转换

---
+++ text-2022-11-1-4-6.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+    int sed, h, m, s;
+    scanf("%d", &sed);
+    if (sed < 60)
+    {
+        h = 0;
+        m = 0;
+        s = sed;
+    }
+    else if (sed < 3600)
+    {
+        s = sed % 60;
+        m = sed / 60;
+        h = 0;
+    }
+    else
+    {
+        s = sed % 60;
+        m = (sed / 60) % 60;
+        h = sed / 3600;
+    }
+    printf("%d %d %d", h, m, s);
+    return 0;
+}
