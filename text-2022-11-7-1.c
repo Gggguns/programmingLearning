5790f18f2a89eb78d16f981586890f895ff60b2d
Gggguns <2523040452@qq.com>
2023-02-12 17:45:04 +0800

    冒泡排序整型

---
+++ text-2022-11-7-1.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+void bubble_sort(int* arr,int sz)
+{
+	int i = 0, j = 0;
+	int tmp = 0;
+	for (i = 0;i < sz - 1;i++)
+	{
+		for (j = 0;j < sz - 1 - i;j++)
+		{
+			if (arr[j]>arr[j + 1])
+			{
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+}
+int main()
+{
+	int arr[] = { 2,3,5,6,10,9,4,5,6,5,8 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int i=0;
+	bubble_sort(arr,sz);
+	for(i=0;i<sz;i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
