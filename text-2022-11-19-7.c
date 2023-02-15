ad81c97bf5b639d7985f5143d15bb2a28f830029
Gggguns <2523040452@qq.com>
2023-02-15 23:09:33 +0800

    使用指针打印数组内容

--- text-2022-11-19-7.c
+++ text-2022-11-19-7.c
@@ -1,13 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<string.h>
-int main()
+void print(int* arr, int sz)
 {
-	int arr[] = { 1,2,3,4,5,6,7,8,9,10 },sz,i;
-	sz = sizeof(arr) / sizeof(arr[0]);
+	int i = 0;
 	for (i = 0;i < sz;i++)
 	{
 		printf("%d ", *(arr + i));
 	}
+}
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 }, sz, i;
+	sz = sizeof(arr) / sizeof(arr[0]);
+	print(arr, sz);
 	return 0;
 }
