3a949c043b1abc517158b00ec50fb9c587d11992
Gggguns <2523040452@qq.com>
2023-02-22 17:18:28 +0800

    打印整数二进制的奇数位和偶数位
    获取一个整数二进制序列中所有的偶数位和奇数位，分别打印出二进制序列

---
+++ text-2022-11-12-5.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int num = 0, sum1= 0, arr1[20]={0}, arr2[20]={0}, i = 0, j = 0, sum2 = 0;
+	scanf("%d", &num);
+	sum1= sizeof(num) * 8;
+	sum2 = sum1;
+	while (sum1)
+	{
+		if(sum1%2==0)
+			if(num<0)
+			{
+				arr1[i] = 1;
+				i++;
+			}
+			else
+			{
+				arr1[i] = 0;
+				i++;
+			}
+		else
+		{
+			if (num < 0)
+			{
+				arr2[j] = 1;
+				j++;
+			}
+			else
+			{
+				arr2[j] = 0;
+				j++;
+			}
+		}
+		sum1--;
+		num <<= 1;
+	}
+	printf("偶数位:\n");
+	for (i = 0;i < sum2 / 2;i++)
+	{
+		printf("%d ", arr1[i]);
+	}
+	printf("\n奇数位;\n");
+	for (i = 0;i < sum2 / 2;i++)
+	{
+		printf("%d ", arr2[i]);
+	}
+	return 0;
+}
