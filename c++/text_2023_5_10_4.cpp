bf012b69c46ff71cdd7d81f8acc2616859cdfcd1
Gggguns <2523040452@qq.com>
2023-05-27 23:00:59 +0800

    累加天数

--- c++/text_2023_5_10_4.cpp
+++ c++/text_2023_5_10_4.cpp
@@ -5,6 +5,16 @@ using namespace std;
 
 int getmothday(int year, int month)
 {
+    while (month > 12)
+    {
+        month = month - 12;
+        year++;
+    }
+    while (month < 12)
+    {
+        month = month + 12;
+        year--;
+    }
     static int arr[13] = { 0,31,28,31,30,31,30 ,31,31 ,30,31,30,31 };
     if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)))
         return arr[month] + 1;
@@ -22,9 +32,29 @@ public:
     {
         Date ret(*this);
         ret._day = ret._day + day;
-        while (ret._day > getmothday(ret._year, ret._month));
+        while (ret._day > getmothday(ret._year, ret._month))
+        {
+            ret._day = ret._day - getmothday(ret._year, ret._month);
+            ret._month++;
+            while (ret._month > 12)
+            {
+                ret._month=ret._month - 12;
+                ret._year++;
+            }
+        }
         return ret;
 
+    }
+    Date(int year = 0, int month = 0, int day = 0)
+        : _year(year)
+        , _month(month)
+        , _day(day)
+    {}
+    Date(const Date& d)
+    {
+        _year = d._year;
+        _month = d._month;
+        _day = d._day;
     }
 private:
     int _year;
