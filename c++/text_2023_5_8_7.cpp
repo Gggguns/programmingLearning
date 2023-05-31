9cedfb2bd0f13ca821e5e889babcf8f447ba67a8
Gggguns <2523040452@qq.com>
2023-05-31 13:07:48 +0800

    计算天数

--- c++/text_2023_5_8_7.cpp
+++ c++/text_2023_5_8_7.cpp
@@ -1,16 +1,46 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 using namespace std;
+int getmonth(int year, int month)
+{
+	static int arr[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+	while (month > 12)
+	{
+		month = month - 12;
+		year++;
+	}
+	while (month < 0)
+	{
+		month = month + 12;
+		year--;
+	}
+	if (month == 2 && ((year % 4 == 0 && year % 100 == 0) || year % 400 == 0))
+	{
+		return arr[month] + 1;
+	}
+	else
+	{
+		return arr[month];
+	}
+}
 class Date
 {
 public:
 	friend istream& operator>>(istream& in, Date& d);
 	friend ostream& operator<<(ostream& out, Date& d);
+	Date operator+(int day)
+	{
+		_day = _day + day;
+		while (_day > getmonth(_year, _month))
+		{
+
+		}
+	}
 	Date(int year, int month, int day)
 		: _year(year)
 		, _month(month)
 		, _day(day)
-	{} 
+	{}
 	Date(const Date& d)
 	{
 		_year = d._year;
@@ -38,6 +68,7 @@ ostream& operator<<(ostream& out, Date& d)
 	out << d._year << '-' << d._month << '-' << d._day << endl;
 	return out;
 }
+
 int main()
 {
 	return 0;
