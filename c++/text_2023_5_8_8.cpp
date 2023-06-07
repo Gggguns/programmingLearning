c2a426fc60722a1bbe85333099e0ad696d6fa167
Gggguns <2523040452@qq.com>
2023-06-07 14:12:41 +0800

    日期差值

--- c++/text_2023_5_8_8.cpp
+++ c++/text_2023_5_8_8.cpp
@@ -1,6 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 using namespace std;
+int getmonthday(int year, int month)
+{
+	static int arr[13] = { 0,31,28,30,31,30,31,31,30,31,30,31 };
+	while (month > 12)
+	{
+		month -= 12;
+		year++;
+	}
+	while (month<0)
+	{
+		month += 12;
+		year--;
+	}
+	if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)))
+		return arr[month] + 1;
+	else
+		return arr[month];
+}
 class Date
 {
 public:
@@ -21,14 +39,131 @@ public:
 		_month = 0;
 		_day = 0;
 	}
-	int operator-(Date& dd)
+
+	//比较大小运算符重载
+	//等于
+	bool operator==(Date& d)
+	{
+		if (_year == d._year && _month == d._month && _day == d._day)
+			return true;
+		else
+			return false;
+	}
+	//大于
+	bool operator>(Date& d)
+	{
+		if (*this == d)
+			return false;
+		else if (_year < d._year)
+			return false;
+		else if (_year == d._year && _month < d._month)
+			return false;
+		else if (_year == d._year && _month == d._month && _day < d._day)
+			return false;
+		else
+			return true;
+	}
+	//大于等于
+	bool operator>=(Date& d)
+	{
+		if ((*this == d) || (*this > d))
+			return true;
+		else
+			return false;
+	}
+	//小于
+	bool operator<(Date& d)
+	{
+		if (*this >= d)
+			return false;
+		else
+			return true;
+	}
+	//小于等于
+	bool operator<=(Date& d)
+	{
+		if (*this > d)
+			return false;
+		else
+		{
+			return true;
+		}
+	}
+	//赋值
+	Date& operator=(const Date& d)
+	{
+		_year = d._year;
+		_month = d._month;
+		_day = d._day;
+		return *this;
+	}
+
+
+	//运算符重载
+	// 相加
+	Date operator+(int day)
+	{
+		Date d(*this);
+		if (day < 0)
+		{
+			day = -day;
+			d = d - day;
+		}
+		d._day += day;
+		while (d._day > getmonthday(d._year, d._month))
+		{
+			d._day -= getmonthday(d._year,d._month);
+			d._month += 1;
+			while (d._month > 12)
+			{
+				d._year++;
+				d._month -= 12;
+			}
+		}
+		return d;
+	}
+	//相减
+	// 
+	Date operator-(int day)
+	{
+		Date d(*this);
+		if (day < 0)
+		{
+			day = -day;
+			d = d + day;
+		}
+		d._day -= day;
+		while (d._day < 0)
+		{
+			d._month--;
+			d._day += getmonthday(d._year, d._month);
+			while (d._month < 1)
+			{
+				d._month += 12;
+				d._year--;
+			}
+		}
+		return d;
+	}
+	//
+	int operator-(Date& d)
 	{
 		int i = 0;
-		Date d(*this);
-		while (d < dd)
+		int sign = -1;
+		Date Max(d);
+		Date Min(*this);
+		if (*this > d)
 		{
-
+			Max = *this;
+			Min = d;
+			sign = -sign;
 		}
+		while ( Min<Max )
+		{
+			Min = Min + 1;
+			i++;
+		}
+		return i * sign;
 	}
 private:
 	int _year;
@@ -37,5 +172,10 @@ private:
 };
 int main()
 {
+	int num1 = 0,num2=0;
+	cin >> num1>>num2;
+	Date d1(num1 / 10000, ((num1 / 100) % 100), num1 % 100);
+	Date d2(num2 / 10000, ((num2 / 100) % 100), num2 % 100);
+	cout << d1 - d2 << endl;
 	return 0;
 }
