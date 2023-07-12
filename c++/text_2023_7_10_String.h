f2e05c8aa961d6b7885a2d27497e78ed38f08df1
Gggguns <2523040452@qq.com>
2023-07-13 07:46:47 +0800

    模拟string

--- c++/text_2023_7_10_String.h
+++ c++/text_2023_7_10_String.h
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
+#include<assert.h>
 using namespace std;
 
 //模拟string使用的命名空间
@@ -8,15 +9,35 @@ namespace zjh
 	class string
 	{
 	public:
-		string(std::size_t size = 0, std::size_t capacity = 4)
-			:_size(size)
-			,_capacity(capacity)
+		string(const char*str) 
 		{
-			char
+			_size = strlen(str);
+			_capacity = _size;
+			_str = reserve(_size);
+			strcpy(_str, str);
+		}
+		char* reserve(size_t size)
+		{
+			assert(size >= 0);
+			char* ret = new char[size + 1];
+			return ret;
+		}
+		string(const string& str) 
+		{
+			_size = str._size;
+			_capacity = str._capacity;
+			_str = new char[str._capacity];
+			strcpy(_str, str._str);
+		}
+		~string()
+		{
+			_size = 0;
+			_capacity = 0;
+			delete(_str);
 		}
 	private:
-		std::size_t _size;
-		std::size_t _capacity;
+		size_t _size;
+		size_t _capacity;
 		char* _str;
 	};
 }
--- c++/text_2023_7_10_Text.cpp
+++ c++/text_2023_7_10_Text.cpp
@@ -1,2 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include"text_2023_7_10_String.h"
+
+int main()
+{
+	return 0;
+}
