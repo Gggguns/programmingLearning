32dfd9f32c52f596396e5d3b973faf23b8d0fc51
Gggguns <2523040452@qq.com>
2023-07-19 23:54:57 +0800

    找出只出现了一次的数

---
+++ c++/text_2023_7_8_5.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int singleNumber(vector<int>& nums)
+    {
+        int num = 0;
+        for (auto n : nums)
+        {
+            num = num ^ n;
+
+        }
+        return num;
+    }
+};
+void text_vector1()
+{
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(2);
+    v.push_back(1);
+    v.push_back(4);
+    v.push_back(5);
+    v.push_back(5);
+    v.push_back(4);
+    v.push_back(6);
+    cout << singleNumber(v) << endl;
+}
+int main()
+{
+    text_vector1();
+    return 0;
+}
