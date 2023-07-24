c575578ecc7c4efb9798c4b14c396cb83175849c
Gggguns <2523040452@qq.com>
2023-07-24 23:22:27 +0800

    模拟vector

--- c++/vector.h
+++ c++/vector.h
@@ -5,10 +5,30 @@ namespace zjy
 	template<class T>
 	class vector
 	{
+	public:
+		vector()
+			:_start(nullptr)
+			,_finish(nullptr)
+			,_endofstorage(nullptr)
+		{}
+		vector(const vector<T>& T = T())
+		{
+			_start = T._start;
+			_finish = T._finish;
+			_endofstorage = T._endofstorage;
+		}
+
+		size_t size()const
+		{
+			assert(_finish&&_start)
+			return _finish - _star;
+		}
 		typedef T* iterator;
 	private:
-		iterator star;
-		iterator finsh;
-		iterator endofshorch;
+		iterator _start;
+		iterator _finish;
+		iterator _endofstorage;
 	};
+	
+
 }
