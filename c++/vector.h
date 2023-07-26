f53d5d344588fa081bf6e10b63dec44098d06831
Gggguns <2523040452@qq.com>
2023-07-26 21:56:46 +0800

    模拟string

--- c++/vector.h
+++ c++/vector.h
@@ -6,24 +6,188 @@ namespace zjy
 	class vector
 	{
 	public:
+		typedef T* iterator;
+		typedef const T* const_iterator;
 		vector()
 			:_start(nullptr)
-			,_finish(nullptr)
-			,_endofstorage(nullptr)
+			, _finish(nullptr)
+			, _endofstorage(nullptr)
 		{}
-		vector(const vector<T>& T = T())
+		vector(size_t n, const T& val = T())
 		{
-			_start = T._start;
-			_finish = T._finish;
-			_endofstorage = T._endofstorage;
+			_start = new T[n](val);
+			_finish = _start + n;
+			_endofstorage=_finish;
+		}
+		//øΩ±¥
+		vector(const vector<T>& v)
+		{
+			size_t len = T.size();
+			if(capacity()<len)
+				reserve(len);
+			int i = 0;
+			for (i = 0;i < len;i++)
+			{
+				_start[i] = v._start[i];
+			}
+			_finish = _start + len;
+		}
+		template<class InputIterator>
+
+		vector(InputIterator first, InputIterator last)
+		{
+			iterator it = begin();
+			while (first != last)
+			{
+				*it = *first;
+				it++;
+				first++;
+			}
+		}
+		vector<T> operator=(const vector<T>& v)
+		{
+			size_t len = v.size();
+			if (capacity() < len)
+				reserve(len);
+			int i = 0;
+			for (i = 0;i < len;i++)
+			{
+				_start[i] = v._start[i];
+			}
+			_finish = _start + len;
+			return *this;
+		}
+		
+		~vector()
+		{
+			delete[]_start;
+			_start = nullptr;
+			_finish = nullptr;
+			_endofstorage = nullptr;
+		}
+		//µ¸¥˙∆˜
+		iterator begin()const
+		{
+			return _start;
 		}
 
+		iterator end()const
+		{
+			return _finish;
+		}
+		const_iterator cbegin()const
+		{
+			return _start;
+		}
+		const_iterator cend()const
+		{
+			return _finish;
+		}
+		//¿©»›
+		void reserve(size_t n)
+		{
+			if (capacity() < n)
+			{
+				iterator tmp = new T[n];
+				int i = 0;
+				while (i<size())
+				{
+					tmp[i] = _start[i];
+				}
+				size_t oldsize = size();
+				delete[] _start;
+				_start = tmp;
+				_finish = tmp + oldsize;
+				_endofstorage = tmp + n;
+			}
+		}
+		void resize(size_t n, const T& value = T())
+		{
+			if (capacity() < n)
+			{
+				iterator tmp = new T[n];
+				delete[] _start;
+				_start = tmp;
+				_endofstorage = _start + n;
+			}
+			int i = 0;
+			for (i = 0;i < n;i++)
+			{
+				_start[i] = value;
+			}
+			_finish = _start + n;
+			
+		}
+		//vectorµƒ¥Û–°
 		size_t size()const
 		{
-			assert(_finish&&_start)
-			return _finish - _star;
+			return _finish - _start;
 		}
-		typedef T* iterator;
+		//vectorµƒ»›¡ø
+		size_t capacity()const
+		{	
+			return _endofstorage - _start;
+		}
+		//∑Ω¿®∫≈‘ÀÀ„∑˚÷ÿ‘ÿ
+		T& operator[](size_t pos)
+		{
+			assert(pos < capacity());
+			return _start[pos];
+		}
+
+		const T& operator[](size_t pos)const
+		{
+			assert(pos < capacity());
+			return _start[pos];
+		}
+		//Œ≤≤Â
+		void push_back(const T& x)
+		{
+			if (capacity() == size())
+				reserve(capacity() * 2);
+			*_finish = x;
+			_finish++;
+		}
+		//Œ≤…æ
+		void pop_back()
+		{
+			_finish--;
+		}
+		//Ωªªª
+		void swap(vector<T>& v)
+		{
+			std::swap(_start, v._start);
+			std::swap(_finish, v._finish);
+			std::swap(_endofstorage, v._endofstorage);
+		}
+		//posŒª÷√≤Â»Î
+		iterator insert(iterator pos, const T& x)
+		{
+			size_t n = pos - begin();
+			if (capacity() == size())
+				reserve(capacity() * 2);
+			iterator end = end();
+			while (end != pos)
+			{
+				*end = *(end-1);
+				end--;
+			}
+			_finish++;
+			*pos = x;
+			return begin() + n;
+		}
+		//posŒª÷√…æ≥˝
+		iterator erase(iterator pos)
+		{
+			iterator end = pos;
+			_finish--;
+			while (end - 1 != _finish)
+			{
+				*end = *(end + 1);
+				end++;
+			}
+			return pos;
+		}
 	private:
 		iterator _start;
 		iterator _finish;
