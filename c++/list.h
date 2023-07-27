6400301d8375475ab596f0b4a782d53d8b33cc47
Gggguns <2523040452@qq.com>
2023-07-27 23:38:19 +0800

    模拟list

---
+++ c++/list.h
@@ -0,0 +1,37 @@
+#pragma once
+#include<iostream>
+namespace zjh
+{
+	template<class T>
+	struct list_node
+	{
+		list_node* prve;
+		list_node* next;
+		T value;
+
+		list_node()
+			:prev(nullptr)
+			,next(nullptr)
+			,value(T())
+		{}
+	};
+	template<class T>
+	class list
+	{
+		typedef list_node<T> Node;
+	public:
+		list()
+		{
+			_head = new Node;
+			_head->next = _head;
+			_head->prve = _head;
+		}
+		~list()
+		{
+			;
+		}
+	private:
+		Node* _head;
+	};
+	
+}
