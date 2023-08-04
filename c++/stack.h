17701bc7837fd10c859ac32157ac1d7b6a775eac
Gggguns <2523040452@qq.com>
2023-08-04 23:59:00 +0800

    模拟stack

--- c++/stack.h
+++ c++/stack.h
@@ -1,9 +1,21 @@
 #pragma once
 #include<iostream>
+#include<list>
+#include<vector>
+#include<deque>
+using namespace std;
 namespace zjh
 {
+	template<class T,class Container=deque<T>>
 	class stack
 	{
-
+	public:
+		void push(T& x)
+		{
+			c.push_back(x);
+		}
+		void pop
+	private:
+		Container c;
 	};
 }
