c7aacc992babed2b35feefbe3c493068121bc167
Gggguns <2523040452@qq.com>
2023-08-08 23:13:21 +0800

    模拟priority_queue

--- c++/priority_queue.h
+++ c++/priority_queue.h
@@ -7,6 +7,28 @@ namespace zjh
 	template<class T,class Container=vector<T>>
 	class priority_queue
 	{
+	public:
+		void push()
+		{
 
+		}
+		void pop()
+		{
+
+		}
+		void size()
+		{
+
+		}
+		void empty()
+		{
+
+		}
+		const T& top()const
+		{
+
+		}
+	private:
+		Container C;
 	};
 }
