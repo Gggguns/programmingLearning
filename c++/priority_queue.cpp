c9e4391da5ef753f1cddcd99c170e2d60c4c57f9
Gggguns <2523040452@qq.com>
2023-08-09 10:44:46 +0800

    模拟priority_queue

--- c++/priority_queue.cpp
+++ c++/priority_queue.cpp
@@ -1,6 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include"priority_queue.h"
+#define N 10
+void textpriority_queue1()
+{
+	zjh::priority_queue<int, vector<int>,zjh::Greater<int>> Q;
+	srand(time(NULL));
+	int i = 0;
+	for (i = 0;i < N;i++)
+	{
+		int ran = rand() / 100;
+		Q.push(ran);
+	}
+	cout << Q.size() << endl;
+	cout << Q.empty() << endl;
+	for (i = 0;i < N;i++)
+	{
+		cout << Q.top() << ' ';
+		Q.pop();
+	}
+}
 int main()
 {
+	textpriority_queue1();
+	//cout << 9 % 2 << endl;
 	return 0;
 }
--- c++/priority_queue.h
+++ c++/priority_queue.h
@@ -1,34 +1,90 @@
 #pragma once
 #include<iostream>
 #include<vector>
+#include<time.h>
 using namespace std;
 namespace zjh
 {
-	template<class T,class Container=vector<T>>
+	template<class T>
+	struct Less
+	{
+		bool compare(const T& x, const T& y)
+		{
+			return x > y;
+		}
+	};
+	template<class T>
+	struct Greater
+	{
+		bool compare(const T& x, const T& y)
+		{
+			return x < y;
+		}
+	};
+	template<class T,class Container=vector<T>,class Compare=Less<T>>
 	class priority_queue
 	{
 	public:
-		void push()
+		void AdjustUp(int child)
 		{
-
+			Compare com;
+			int parent = (child - 1) / 2;
+			while (child > 0)
+			{
+				if ((child + 1) < _con.size() && com.compare(_con[child] , _con[child + 1]))
+				{
+					child++;
+				}
+				if (com.compare(_con[parent] , _con[child]))
+				{
+					std::swap(_con[parent] , _con[child]);
+				}
+				child = parent;
+				parent = (child - 1) / 2;
+			}
+		}
+		void AdjustDown(int parent)
+		{
+			Compare com;
+			int child = parent * 2 + 1;
+			while (child < _con.size())
+			{
+				if ((child + 1) < _con.size() && com.compare(_con[child] , _con[child + 1]))
+				{
+					child++;
+				}
+				if (com.compare(_con[parent] , _con[child]))
+				{
+					std::swap(_con[parent], _con[child]);
+				}
+				parent = child;
+				child = parent * 2 + 1;
+			}
+		}
+		void push(const T& value)
+		{
+			_con.push_back(value);
+			AdjustUp(_con.size()-1);
 		}
 		void pop()
 		{
-
+			std::swap(_con[0], _con[_con.size() - 1]);
+			_con.pop_back();
+			AdjustDown(0);
 		}
-		void size()
+		size_t size()
 		{
-
+			return _con.size();
 		}
-		void empty()
+		bool empty()
 		{
-
+			return _con.size() == 0;
 		}
 		const T& top()const
 		{
-
+			return _con[0];
 		}
 	private:
-		Container C;
+		Container _con;
 	};
 }
