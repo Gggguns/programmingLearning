666bd0d034a0bc37cbd42da6312223e2ce5113bd
Gggguns <2523040452@qq.com>
2023-08-23 22:23:02 +0800

    模拟搜索二叉树

--- c++/BinarySearchTree.cpp
+++ c++/BinarySearchTree.cpp
@@ -3,7 +3,7 @@
 #define N 10
 void testBST1()
 {
-	zjh::BinarySearchTree<int> T;
+	Key::BinarySearchTree<int> T;
 	srand(time(NULL));
 	int arr[N] = {1,4,3,2,5,6,78,9,8,8};
 	
@@ -29,8 +29,32 @@ void testBST1()
 	}
 	
 }
+void testBST2()
+{
+	Key::BinarySearchTree<int> T;
+	int arr[] = { 10,2,30,4,15,46,18,10,1,12,17,12 };
+	for (auto e : arr)
+	{
+		T.InsertR(e);
+	}
+	Key::BinarySearchTree<int> TT;
+	TT = T;
+	T.Inorder();
+	cout << endl;
+	TT.Inorder();
+	cout << endl;
+	/*cout<<T.FindR(80)<<endl;
+	for (auto e : arr)
+	{
+		T.eraseR(e);
+		T.Inorder();
+		cout << endl;
+	}*/
+	
+	
+}
 int main()
 {
-	testBST1();
+	testBST2();
 	return 0;
 }
--- c++/BinarySearchTree.h
+++ c++/BinarySearchTree.h
@@ -4,7 +4,7 @@
 #include<time.h>
 using namespace std;
 
-namespace zjh
+namespace Key
 {
 	template<class K>
 	struct BinarySearchTreeNode
@@ -18,6 +18,10 @@ namespace zjh
 			, _right(nullptr)
 		{}
 	};
+
+
+
+
 	template<class K>
 	class BinarySearchTree
 	{
@@ -26,7 +30,22 @@ namespace zjh
 		BinarySearchTree()
 			:_root(nullptr)
 		{}
+		BinarySearchTree(const BinarySearchTree& BST)
+		{
+			_root = copy(BST._root);
+		}
+		~BinarySearchTree()
+		{
+			Destroy(_root);
+			_root = nullptr;
+		}
 
+		//赋值重载
+		BinarySearchTree& operator=(BinarySearchTree BST)
+		{
+			swap(BST._root, _root);
+			return *this;
+		}
 		//插入
 		bool Insert(const K& key)
 		{
@@ -68,21 +87,17 @@ namespace zjh
 			return true;
 			
 		}
+		//递归插入
+		bool InsertR(const K&key)
+		{
+			return _InsertR(_root, key);
+		}
 		//中序遍历
 		void Inorder()
 		{
 			_Inorder(_root);
 		}
-		void _Inorder(BSTreeNode* root)
-		{
-			if (root==NULL)
-			{
-				return;
-			}
-			_Inorder(root->_left);
-			cout << root->_key << ' ';
-			_Inorder(root->_right);
-		}
+		
 		//删除
 		bool erase(const K& key)
 		{
@@ -157,6 +172,11 @@ namespace zjh
 			}
 			return false;
 		}
+		//递归删除
+		bool eraseR(const K& key)
+		{
+			return _eraseR(_root, key);
+		}
 		//查找
 		bool Find(const K& key)
 		{
@@ -178,8 +198,289 @@ namespace zjh
 			}
 			return false;
 		}
+		//递归查找
+		bool FindR(const K& key)
+		{
+			return _FindR(_root, key);
+		}
 	private:
+		//拷贝二叉搜索树
+		BSTreeNode* copy(BSTreeNode* root)
+		{
+			if (root == nullptr)
+				return nullptr;
+			BSTreeNode* copyroot = new BSTreeNode(root->_key);
+			copyroot->_left = copy(root->_left);
+			copyroot->_right = copy(root->_right);
+			return copyroot;
+		}
+		//析构的子函数
+		void Destroy(BSTreeNode* root)
+		{
+			if (root == nullptr)
+			{
+				return;
+			}
+			Destroy(root->_left);
+			Destroy(root->_right);
+			delete root;
+		}
+		//递归删除的子函数
+		bool _eraseR(BSTreeNode*& root, const K& key)
+		{
+			if (root == nullptr)
+			{
+				return false;
+			}
+			if (root->_key > key)
+			{
+				_eraseR(root->_left, key);
+			}
+			else if (root->_key < key)
+			{
+				_eraseR(root->_right, key);
+			}
+			else
+			{
+				
+				if (root->_left == nullptr)
+					root = root->_right;
+				else if (root->_right == nullptr)
+					root = root->_left;
+				else
+				{
+					BSTreeNode* leftMax = root->_left;
+					while(leftMax->_right)
+					{
+						leftMax = leftMax->_right;
+					}
+					swap(root->_key, leftMax->_key);
+					_eraseR(root->_left, key);
+					delete leftMax;
+				}
+				
+			}
+		}
+		//递归查找的子函数
+		bool _FindR(BSTreeNode* root, const K& key)
+		{
+			if (root == nullptr)
+			{
+				return false;
+			}
+			if (root->_key > key)
+			{
+				_FindR(root->_left, key);
+			}
+			else if (root->_key < key)
+			{
+				_FindR(root->_right, key);
+			}
+			else
+			{
+				return true;
+			}
+		}
+		//递归插入的子函数
+		bool _InsertR(BSTreeNode*& root, const K& key)
+		{
+			if (root == nullptr)
+			{
+				root = new BSTreeNode(key);
+				return true;
+			}
+			if (root->_key > key)
+				_InsertR(root->_left, key);
+			else if (root->_key < key)
+			{
+				_InsertR(root->_right, key);
+			}
+			else
+			{
+				return false;
+			}
+		}
+		//中序遍历子函数
+		void _Inorder(BSTreeNode* root)
+		{
+			if (root == NULL)
+			{
+				return;
+			}
+			_Inorder(root->_left);
+			cout << root->_key << ' ';
+			_Inorder(root->_right);
+		}
 		BSTreeNode* _root;
+
 	};
 }
 
+namespace Key_value
+{
+	template<class K>
+	struct BinarySearchTreeNode
+	{
+		K _key;
+		BinarySearchTreeNode* _left;
+		BinarySearchTreeNode* _right;
+		BinarySearchTreeNode(const K& key)
+			:_key(key)
+			, _left(nullptr)
+			, _right(nullptr)
+		{}
+	};
+
+	template<class K>
+	class BinarySearchTree
+	{
+		typedef BinarySearchTreeNode<K> BSTreeNode;
+	public:
+		BinarySearchTree()
+			:_root(nullptr)
+		{}
+		BinarySearchTree(const BSTreeNode& root)
+		{
+			_root = copy(root);
+		}
+		~BinarySearchTree()
+		{
+			Destroy(_root);
+			_root = nullptr;
+		}
+		
+		//递归插入
+		bool InsertR(const K& key)
+		{
+			return _InsertR(_root, key);
+		}
+
+		//中序遍历
+		void Inorder()
+		{
+			_Inorder(_root);
+		}
+				
+		//递归删除
+		bool eraseR(const K& key)
+		{
+			return _eraseR(_root, key);
+		}
+
+		//递归查找
+		bool FindR(const K& key)
+		{
+			return _FindR(_root, key);
+		}
+	private:
+		//拷贝二叉搜索树
+		BSTreeNode* copy(BSTreeNode* root)
+		{
+			if (root == nullptr)
+				return nullptr;
+			BSTreeNode* copyroot = new BSTreeNode(root->_key);
+			copyroot->_left = copy(root->_left);
+			copyroot->_right = copy(root->_right);
+			return copyroot;
+		}
+		//析构的子函数
+		void Destroy(BSTreeNode* root)
+		{
+			if (root == nullptr)
+			{
+				return;
+			}
+			Destroy(root->_left);
+			Destroy(root->_right);
+			delete root;
+		}
+		//递归删除的子函数
+		bool _eraseR(BSTreeNode*& root, const K& key)
+		{
+			if (root == nullptr)
+			{
+				return false;
+			}
+			if (root->_key > key)
+			{
+				_eraseR(root->_left, key);
+			}
+			else if (root->_key < key)
+			{
+				_eraseR(root->_right, key);
+			}
+			else
+			{
+
+				if (root->_left == nullptr)
+					root = root->_right;
+				else if (root->_right == nullptr)
+					root = root->_left;
+				else
+				{
+					BSTreeNode* leftMax = root->_left;
+					while (leftMax->_right)
+					{
+						leftMax = leftMax->_right;
+					}
+					swap(root->_key, leftMax->_key);
+					_eraseR(root->_left, key);
+					delete leftMax;
+				}
+
+			}
+		}
+		//递归查找的子函数
+		bool _FindR(BSTreeNode* root, const K& key)
+		{
+			if (root == nullptr)
+			{
+				return false;
+			}
+			if (root->_key > key)
+			{
+				_FindR(root->_left, key);
+			}
+			else if (root->_key < key)
+			{
+				_FindR(root->_right, key);
+			}
+			else
+			{
+				return true;
+			}
+		}
+		//递归插入的子函数
+		bool _InsertR(BSTreeNode*& root, const K& key)
+		{
+			if (root == nullptr)
+			{
+				root = new BSTreeNode(key);
+				return true;
+			}
+			if (root->_key > key)
+				_InsertR(root->_left, key);
+			else if (root->_key < key)
+			{
+				_InsertR(root->_right, key);
+			}
+			else
+			{
+				return false;
+			}
+		}
+		//中序遍历子函数
+		void _Inorder(BSTreeNode* root)
+		{
+			if (root == NULL)
+			{
+				return;
+			}
+			_Inorder(root->_left);
+			cout << root->_key << ' ';
+			_Inorder(root->_right);
+		}
+		BSTreeNode* _root;
+
+	};
+}
