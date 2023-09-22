260b43685430eb2396ad42157cec8cdab91ba142
Gggguns <2523040452@qq.com>
2023-09-22 14:21:23 +0800

    将第二个字符串出现在第一个字符串中的字符删除

---
+++ c++/text_2023_9_22_exam_2.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string str1, str2;
+    getline(cin, str1);
+    getline(cin, str2);
+    //É¾³ý³öÏÖµÄ×Ö·û
+    int i = 0;
+    for (auto e : str2)
+    {
+        for (i = str1.find(e);i != string::npos;i = str1.find(e))
+        {
+            str1.erase(str1.begin() + i);
+        }
+    }
+    cout << str1;
+}
