e8e23c88d29b459e605410e4e2689d793e72e029
Gggguns <2523040452@qq.com>
2023-10-31 20:42:35 +0800

    星际秘密

--- c++/text_2023_10_30_exam_1.cpp
+++ c++/text_2023_10_30_exam_1.cpp
@@ -1,7 +1,33 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<iostream>
+#include <iostream>
+#include<vector>
 using namespace std;
-int main()
-{
-	return 0;
+
+int main() {
+    int k = 0;
+    while (cin >> k)
+    {
+
+        while (k--)
+        {
+            vector<vector<long long>> vv1(2, vector<long long>(2, 0));
+            vv1[0][0] = vv1[1][0] = vv1[0][1] = 1;
+            vv1[1][1] = 0;
+            vector<vector<long long>> vv2(vv1);
+            vector<vector<long long>> vv3(vv1);
+            int time = 0;
+            cin >> time;
+            while (--time)
+            {
+                vv3[0][0] = (vv2[0][0] * vv1[0][0] + vv2[0][1] * vv1[1][0]) % 10000;
+                vv3[0][1] = (vv2[0][0] * vv1[0][1] + vv2[0][1] * vv1[1][1]) % 10000;
+                vv3[1][0] = (vv2[1][0] * vv1[0][0] + vv2[1][1] * vv1[1][0]) % 10000;
+                vv3[1][1] = (vv2[1][0] * vv1[0][1] + vv2[1][1] * vv1[1][1]) % 10000;
+                vv2.swap(vv3);
+            }
+            printf("%.4lld", vv2[0][0] % 10000);
+
+        }
+        cout << endl;
+    }
 }
