2f409e58abe4b62604e55e3c3bb38ac2ed2e867e
Gggguns <2523040452@qq.com>
2024-03-03 21:32:39 +0800

    买卖股票的最佳时期含⼿续费

---
+++ c++/text_2024_3_3_dp_16.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        //创建dp表
+        //初始化
+        //填表
+        //返回值
+        int n = prices.size();
+        if (n < 2)return 0;
+        vector<vector<int>> dp(n, vector<int>(2));
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+        for (int i = 1;i < n;i++)
+        {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+            dp[i][1] = max(dp[i - 1][0] + prices[i] - fee, dp[i - 1][1]);
+        }
+        return max(dp[n - 1][0], dp[n - 1][1]);
+    }
+};
+int main()
+{
+	return 0;
+}
