4358f7fc957066ea3f2c432ed2a40e335049f39e
Gggguns <2523040452@qq.com>
2024-05-15 23:18:18 +0800

    基本计算器||

--- c++/text_2024_5_14_op_67.cpp
+++ c++/text_2024_5_14_op_67.cpp
@@ -1,7 +1,38 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
-#include<stack>
+#include<vector>
 using namespace std;
+class Solution {
+public:
+    int calculate(string s) {
+        long long sz = s.size();
+        vector<long long> st;
+        long long i = 0;
+        char op = '+';
+        while (i < sz) {
+            if (s[i] == ' ')i++;
+            else if (s[i] >= '0' && s[i] <= '9') {
+                long long num = 0;
+                while (s[i] >= '0' && s[i] <= '9') {
+                    num = num * 10 + s[i++] - '0';
+                }
+                if (op == '+')st.push_back(num);
+                else if (op == '*')st.back() *= num;
+                else if (op == '/')st.back() /= num;
+                else if (op == '-')st.push_back(-num);
+            }
+            else {
+                op = s[i];
+                i++;
+            }
+        }
+        long long Ret = 0;
+        for (auto e : st)Ret += e;
+        return Ret;
+    }
+};
 int main() {
+    Solution s;
+    cout<<s.calculate(" 3/2 ");
 	return 0;
 }
