39c237df4dfe31b28d058278269b79ba84c40d17
Gggguns <2523040452@qq.com>
2025-02-12 10:48:47 +0800

    Encapsulated native thread

---
+++ c++/text_2025_2_12_Thread/Main.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <unistd.h>
+#include <cstdio>
+#include <vector>
+#include "Thread.hpp"
+
+void Print()
+{
+    while(true)
+    {
+        printf("haha, 我是一个封装线程...\n");
+        sleep(1);
+    }
+}
+
+int main()
+{
+    std::vector<Thread> threads;
+    for(int i = 0; i < 10; i++)
+    {
+        threads.push_back(Thread(Print));
+    }
+
+    for(auto &t : threads)
+    {
+        t.Run();
+    }
+
+    for(auto &t : threads)
+    {
+        t.Join();
+    }
+
+
+    // Thread t(Print);
+    // t.Run();
+
+    // std::cout << "是否启动成功：" << t.IsRunning() << std::endl;
+    // std::cout << "启动成功时间戳：" << t.StartTimeStamp() << std::endl;
+    // std::cout << "线程的名字：" << t.Name() << std::endl;
+
+    // t.Join();
+    return 0;
+}
