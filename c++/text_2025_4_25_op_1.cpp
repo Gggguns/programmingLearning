bd27da78067f943b32acf60dac0e061fc95d5dbf
jianhua <2523040452@qq.com>
2025-04-25 22:07:59 +0800

    Find Minimum in Rotated Sorted Array

---
+++ c++/text_2025_4_25_op_1.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+        int mid = (left + right) / 2;
+        while (left < right)
+        {
+            mid = (left + right) / 2;
+            if (nums[mid] >= nums[left]) left = mid + 1;
+            else right = mid;
+        }
+        return nums[left];
+    }
+};
+int main()
+{
+    return 0;
+}
