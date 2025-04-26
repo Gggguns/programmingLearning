5a30c4b9b3bfd165ca75e2c82c4172d013495b4d
jianhua <2523040452@qq.com>
2025-04-26 20:41:52 +0800

    Find Minimum in Rotated Sorted Array

--- c++/text_2025_4_25_op_1.cpp
+++ c++/text_2025_4_25_op_1.cpp
@@ -10,7 +10,7 @@ public:
         while (left < right)
         {
             mid = (left + right) / 2;
-            if (nums[mid] >= nums[left]) left = mid + 1;
+            if (nums[mid] >= nums[right]) left = mid + 1;
             else right = mid;
         }
         return nums[left];
