579b5a23ab0694bea73d9c314cf9360b25f0029c
jianhua <2523040452@qq.com>
2025-05-21 20:27:36 +0800

    Reverse Pairs

---
+++ c++/text_2025_5_21_op_1.cpp
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int reversePairs(vector<int>& nums) {
+        int size = nums.size();
+        int left = 0, right = size - 1;
+        temp.resize(size);
+        mergeSort(nums, left, right);
+        return ret;
+    }
+    void mergeSort(vector<int>& nums, int left, int right)
+    {
+        if (left >= right) return;
+        int mid = (left + right) / 2;
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid + 1, right);
+        int start1 = left, end1 = mid + 1;
+        int start2 = mid + 1, end2 = right + 1;
+        int end = left;
+        // ºÏ²¢
+        while (start1 < end1 && start2 < end2)
+        {
+            if (nums[start1] > nums[start2])
+            {
+                if (nums[start1] > nums[start2]*2) ret += (end1 - start1);
+                temp[end++] = nums[start2++];
+            }
+            else temp[end++] = nums[start1++];
+        }
+        while (start1 < end1) temp[end++] = nums[start1++];
+        while (start2 < end2) temp[end++] = nums[start2++];
+
+        // ¿½±´
+        for (int i = left; i <= right; i++) nums[i] = temp[i];
+        int x = 0;
+
+    }
+    int ret = 0;
+    vector<int> temp;
+};
+int main()
+{
+    Solution s;
+    vector<int> nums = { 1,3,2,3,1 };
+    cout << s.reversePairs(nums);
+	return 0;
+}
