35adf090a6fa70dd8b255f34af661e7db19ad0d8
jianhua <2523040452@qq.com>
2025-05-24 22:44:05 +0800

    Reorder List

---
+++ c++/text_2025_5_24_op_1.cpp
@@ -0,0 +1,76 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+ 
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        ListNode* slow = head;
+        if (slow == nullptr) return;
+        ListNode* fast = head->next;
+        // 找中间节点
+        while (fast)
+        {
+            slow = slow->next;
+            fast = fast->next;
+            if (fast)fast = fast->next;
+        }
+
+        // 逆转中间节点之后的链表
+        ListNode* head1 = head;
+        ListNode* head2 = new ListNode();
+        ListNode* cur = slow->next;
+        slow->next = nullptr;
+        ListNode* next = nullptr;
+        while (cur)
+        {
+            next = cur->next;
+            cur->next = head2->next;
+            head2->next = cur;
+            cur = next;
+        }
+        ListNode* rethead = new ListNode();
+        ListNode* des = head2;
+        ListNode* tail = rethead;
+        head2 = head2->next;
+        delete des;
+        des = nullptr;
+        while (head1 && head2)
+        {
+            tail->next = head1;
+            tail = tail->next;
+            tail->next = head2;
+            tail = tail->next;
+            head1 = head1->next;
+            head2 = head2->next;
+        }
+        while (head1)
+        {
+            tail->next = head1;
+            tail = tail->next;
+            head1 = head1->next;
+        }
+        while (head2)
+        {
+            tail->next = head2;
+            tail = tail->next;
+            head2 = head2->next;
+        }
+        des = rethead;
+        rethead = rethead->next;
+        delete des;
+        head = rethead;
+    }
+};
+int main()
+{
+    return 0;
+}
